8fa490979c95afa0d36c28e118a892695c07bf5b
calvofede <calvofede@gmail.com>
2016-01-22 13:45:24 -0300

    mockito

--- Topic 2/practice1/src/test/java/com/topic2/app/AppTest.java
+++ Topic 2/practice1/src/test/java/com/topic2/app/AppTest.java
@@ -3,6 +3,8 @@ package com.topic2.app;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.mockito.Mockito.*;
+
 
 /**
  * Unit test for simple App.
@@ -34,5 +36,17 @@ public class AppTest
     public void testApp()
     {
         assertTrue( true );
+    }
+    
+    @Test
+    public void test1()  {
+      //  create mock
+      MyClass test = Mockito.mock(MyClass.class);
+      
+      // define return value for method getUniqueId()
+      when(test.getUniqueId()).thenReturn(43);
+      
+      // use mock in test.... 
+      assertEquals(test.getUniqueId(), 43);
     }
 }
