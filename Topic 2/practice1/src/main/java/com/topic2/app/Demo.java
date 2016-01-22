4b147ee64f963a5cc4894865858dfb19f9c102d1
calvofede <calvofede@gmail.com>
2016-01-22 18:28:09 -0300

    practice2

---
+++ Topic 2/practice1/src/main/java/com/topic2/app/Demo.java
@@ -0,0 +1,24 @@
+package com.topic2.app;
+
+import java.util.ArrayList;
+
+public class Demo {
+
+	 public static void main(String[] args) {
+		 
+		ArrayList<Entry> blog= Blog.getInstance();
+		Blog bl = new Blog();
+	    ActionsEntry ae = new ActionsEntry();
+	    bl.setActionsEntry(ae);
+	    int a=bl.saveMessage();
+	    System.out.println(a);
+	    
+	    
+	    	    
+	    bl.list10MoreRecentEntries();
+	   
+	    
+	 }
+
+	
+}
