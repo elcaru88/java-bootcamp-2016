1baac327d6e19343748a2c278ac6d0dbbc98f565
calvofede <calvofede@gmail.com>
2016-02-02 23:26:56 -0300

    Signed-off-by: calvofede <calvofede@gmail.com>

--- Topic4/src/main/java/com/mycompany/app/Demo.java
+++ Topic4/src/main/java/com/mycompany/app/Demo.java
@@ -1,13 +1,39 @@
 package com.mycompany.app;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Demo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		
 		dbconn conn = new dbconn();
 		
-		conn.
-
+		
+		PreparedStatement stmt = conn.conectar().prepareStatement("SELECT * FROM teacher where idteacher=?");
+		PreparedStatement stmt2 = conn.conectar().prepareStatement("SELECT * FROM course where idteacher=?");
+		
+		stmt.setString(1,"3");
+		stmt2.setString(1,"3");
+		
+		ResultSet rs = stmt.executeQuery();
+		ResultSet rs2 = stmt2.executeQuery();
+		
+		while(rs.next()) {
+			
+			System.out.println("Teacher: "+rs.getString("lastname")+","+rs.getString("firstname"));
+			System.out.println("Course name: "+rs2.getString("name"));
+			System.out.println("Days: "+rs2.getString("days"));
+			System.out.println("From : "+rs2.getString("startTime"));
+			System.out.println("To: "+rs2.getString("endTime"));
+		}
+		
+		rs.close();
+		rs2.close();
+		stmt.close();
+		stmt2.close();
+		
 	}
 
 }
